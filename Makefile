71625a8e817f20267f041d7f30b77da702df9b85
Mark Sellors <github@5vcc.com>
2020-12-23 23:25:18 +0000

    keeps daily pages

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ focal-deps:
 	R -e 'install.packages("rmarkdown", repos = "https://packagemanager.rstudio.com/cran/__linux__/focal/latest")'
 
 docs: cran-usage.Rmd
-	R -e 'rmarkdown::render("cran-usage.Rmd", output_file="index.html", output_dir="docs")'
+	R -e 'rmarkdown::render("cran-usage.Rmd", output_file="cran-usage.html", output_dir="docs")'
+  Rscript dir-control.R
 
 .PHONY: docs focal-deps
---
+++ dir-control.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+# directory control
+
+daily_rmd <- "docs/cran-usage.Rmd"
+
+main_index <- "docs/index.html"
+
+if ( !file.exists(daily_rmd)){
+  stop(paste0("Output file does not exist: ", daily_rmd))
+}
+
+if (file.exists(main_index)){
+  file.remove(main_index)
+}
+
+year <- format(Sys.time(), "%Y")
+month <- format(Sys.time(), "%m")
+day <- format(Sys.Date()-1, "%d")
+
+path <- paste0("docs/", year, "/", month, "/", day)
+
+dir.create(path, recursive = TRUE)
+
+target_file <- paste0(path, "/index.html")
+
+file.rename(daily_rmd, target_file)
+
+file.symlink(target_file, main_index)
