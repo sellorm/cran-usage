b3156b5b18a604a82486b191cdc1db590f492295
Mark Sellors <github@5vcc.com>
2020-12-24 20:13:09 +0000

    ci fixes

--- dir-control.R
+++ dir-control.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 # directory control
 
-daily_rmd <- "docs/cran-usage.html"
+daily_rmd <- paste0(getwd(), "/docs/cran-usage.html")
 
-main_index <- "docs/index.html"
+main_index <- paste0(getwd(), "/docs/index.html")
 
 if ( !file.exists(daily_rmd)){
   stop(paste0("Output file does not exist: ", daily_rmd))
@@ -17,7 +17,7 @@ year <- format(Sys.time(), "%Y")
 month <- format(Sys.time(), "%m")
 day <- format(Sys.Date()-1, "%d")
 
-path <- paste0("docs/", year, "/", month, "/", day)
+path <- paste0(getwd(), "/docs/", year, "/", month, "/", day)
 
 dir.create(path, recursive = TRUE)
 
@@ -25,4 +25,4 @@ target_file <- paste0(path, "/index.html")
 
 file.rename(daily_rmd, target_file)
 
-file.link(target_file, main_index)
+file.copy(target_file, main_index)
