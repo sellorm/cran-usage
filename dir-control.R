6ecd8135b7e271492f0a39e1b13cd452e92dabb6
Mark Sellors <github@5vcc.com>
2021-01-15 11:23:29 +0000

    formatting

--- dir-control.R
+++ dir-control.R
@@ -5,17 +5,17 @@ daily_rmd <- paste0(getwd(), "/docs/cran-usage.html")
 
 main_index <- paste0(getwd(), "/docs/index.html")
 
-if ( !file.exists(daily_rmd)){
+if (!file.exists(daily_rmd)) {
   stop(paste0("Output file does not exist: ", daily_rmd))
 }
 
-if (file.exists(main_index)){
+if (file.exists(main_index)) {
   file.remove(main_index)
 }
 
 year <- format(Sys.time(), "%Y")
 month <- format(Sys.time(), "%m")
-day <- format(Sys.Date()-1, "%d")
+day <- format(Sys.Date() - 1, "%d")
 
 path <- paste0(getwd(), "/docs/", year, "/", month, "/", day)
 
